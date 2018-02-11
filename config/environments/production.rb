f35fa44efdc54814954aebaa7230d3159099399d
Tom Mornini <tom@subledger.com>
2018-02-11 11:02:02 -0800

    improve config files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,12 +10,12 @@ Discourse::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_files = GlobalSetting.serve_static_assets
+  config.serve_static_files = true
 
   config.assets.js_compressor = :uglifier
 
   # stuff should be pre-compiled
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,12 @@
 if ENV['RAILS_ENV']=='production'
 
   # First, you need to change these below to your situation.
-  APP_ROOT = '/home/discourse/discourse'
+  APP_ROOT = '/home/anova'
   num_workers = ENV["NUM_WEBS"].to_i > 0 ? ENV["NUM_WEBS"].to_i : 4
 
   # Second, you can choose how many threads that you are going to run at same time.
   workers "#{num_workers}"
-  threads 8,32
+  threads 8,8
 
   # Unless you know what you are changing, do not change them.
   bind  "unix://#{APP_ROOT}/tmp/sockets/puma.sock"
